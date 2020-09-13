c30c113c620061b55b438e4714ab6054d04fa97e
FelipeEnne <felipeenne@gmail.com>
2020-09-13 02:44:06 -0300

    config router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './Routes';
 
 const App = (props) => {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   );
 }
 
