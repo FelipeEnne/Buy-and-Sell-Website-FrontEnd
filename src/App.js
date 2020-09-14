82e63af6050a6336880bce8854cca27ee37ff8af
FelipeEnne <felipeenne@gmail.com>
2020-09-14 13:04:16 -0300

    create template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './Routes';
+import { Template } from './components/MainComponents';
+import Header from './components/patials/Header';
+import Footer from './components/patials/Footer';
 
 const App = (props) => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Template>
+        <Header />
+
+        <Routes />
+
+        <Footer />
+      </Template>
     </BrowserRouter>
   );
 }
---
+++ src/components/MainComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Template = styled.div``;
+
+const PageContainer = styled.div``;
+
+const PageTitle = styled.h1``;
+
+const PageBody = styled.div``;
+
+
+export { Template, PageContainer, PageTitle, PageBody };
