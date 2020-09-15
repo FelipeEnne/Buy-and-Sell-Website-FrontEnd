3a974d2c6fa600e75ec5aaa9c5a49d6486efb822
FelipeEnne <felipeenne@gmail.com>
2020-09-15 14:48:13 -0300

    add style and logo to header

--- src/components/patials/Header/index.js
+++ src/components/patials/Header/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderArea from './styled';
 
 const Header = () => (
   <HeaderArea>
-    ...
+    <div className="container">
+      <div className="logo">
+        <Link to="/">
+            <span className="logoB">B</span>
+            <span className="logoE">&</span>
+            <span className="logoS">S</span>
+        </Link>
+      </div>
+    </div>
   </HeaderArea>
 );
 
--- src/components/patials/Header/styled.js
+++ src/components/patials/Header/styled.js
@@ -1,8 +1,38 @@
 import styled from 'styled-components';
 
 const HeaderArea = styled.div`
-    height: 100px;
-    background-color:#CCC
+background-color:#FFF;
+height:60px;
+border-bottom:1px solid #CCC;
+
+.container {
+    max-width:1000px;
+    margin:auto;
+    display:flex;
+}
+
+a {
+    text-decoration:none;
+}
+
+.logo {
+    flex:1;
+    display:flex;
+    align-items:center;
+    height:60px;
+
+    .logoB,
+    .logoE,
+    .logoS {
+        font-size:27px;
+        font-weight:bold;
+    }
+
+    .logoB { color:#ff7272 }
+    .logoE { color:#5a5a5a }
+    .logoS { color:#6969f1 }
+
+}
 `;
 
 export default HeaderArea;
