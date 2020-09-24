96fe85697f76961098a8730cf3f775a2df37004c
FelipeEnne <felipeenne@gmail.com>
2020-09-24 15:26:26 -0300

    create the search bar for homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PageArea, SearchArea } from './styled';
 
-const Home = () => (
-        <div>Home</div>
-);
+import useApi from '../../helpers/BSAPI';
+import { PageContainer } from '../../components/MainComponents';
 
-export default Home;
+const Page = () => {
+  const api = useApi();
+
+  return (
+  <>
+    <SearchArea>
+      <PageContainer>
+        <div className='searchBox'>
+          <form method='GET' action='/ads'>
+            <input type='text' name='q' placeholder='What are you looking for?'/>
+            <select name='state'>
+
+            </select>
+            <button>Search</button>
+          </form>
+        </div>
+        <div className='categoryList'>
+
+        </div>
+      </PageContainer>
+    </SearchArea>
+    <PageContainer>
+      <PageArea>
+        ...
+      </PageArea>
+    </PageContainer>
+  </>
+  );
+};
+
+
+export default Page;
