5f0ad94dc8773a4f179d11f85a3fb70c7f089691
FelipeEnne <felipeenne@gmail.com>
2020-11-09 22:05:39 -0300

    add style to ads page

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import PageArea from './styled';
 
 import useApi from '../../helpers/BSAPI';
@@ -8,6 +8,13 @@ import AdItem from '../../components/patials/AdItem';
 
 const Page = () => {
   const api = useApi();
+  const useQueryString = () => new URLSearchParams(useLocation().search);
+
+  const query = useQueryString();
+
+  const [q, setQ] = useState(query.get('q') !== null ? query.get('q') : '');
+  const [cat, setCat] = useState(query.get('cat') !== null ? query.get('cat') : '');
+  const [getState, setGetState] = useState(query.get('state') !== null ? query.get('state') : '');
 
   const [stateList, setStateList] = useState([]);
   const [categories, setCategories] = useState([]);
@@ -45,10 +52,10 @@ const Page = () => {
       <PageArea>
         <div className='leftSide'>
           <form method='GET'>
-            <input type='text' name='q'/>
+            <input type='text' name='q' value={q} placeholder='What are you looking for?'/>
 
             <div className='filterName'>Estado:</div>
-            <select name='state'>
+            <select name='state' value={getState}>
               <option></option>
               {stateList.map((i, k) => (
                 <option key={k} value={i.name}>{i.name}</option>
@@ -58,7 +65,7 @@ const Page = () => {
             <div className='filterName'>Categoria:</div>
             <ul>
                 {categories.map((i, k) => (
-                  <li key={k} className='categoryItem'>
+                  <li key={k} className={cat === i.slug ? 'categoryItem active' : 'categoryItem'}>
                     <img src={i.img} alt=''/>
                     <span>{i.name}</span>
                   </li>
