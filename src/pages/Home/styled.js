656239af2fdd53677330b12cafd9f1fff771807e
FelipeEnne <felipeenne@gmail.com>
2020-12-08 22:31:04 -0300

    add responsiviness to home page

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -73,6 +73,28 @@ const SearchArea = styled.div`
             }
         }
     }
+
+    @media (max-width: 600px)  {
+        .searchBox form{
+            flex-direction: column;
+
+            input {
+                padding:10px;
+                margin-right:0;
+                margin-bottom:10px;
+            }
+
+            select {
+                width:100%;
+                margin-bottom:10px;
+            }
+        }
+    }
+
+    .categoryList .categoryItem{
+        width:50%;
+    }
+
 `;
 
 const PageArea = styled.div`
@@ -94,6 +116,16 @@ const PageArea = styled.div`
         font-weight:bold;
         display:inline-block;
         margin-top:10px;
+    }
+
+    @media (max-width: 600px) {
+        & {
+            margin:10px;
+        }
+
+        .list .adItem {
+            width:50%;
+        }
     }
 `;
 
